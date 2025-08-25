80e0a15b622b7892c0bcf69fd30d5322cab5e178
Shegs.emma <remi.emma04@gmail.com>
2025-08-25 09:10:39 +0100

    rest api code

---
+++ cmd/api/server.go
@@ -0,0 +1,64 @@
+package main
+
+// first run (go mod init restapi)
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	mw "restapi/internal/api/middlewares"
+	"restapi/internal/api/router"
+	"restapi/internal/repository/sqlconnect"
+
+	"github.com/joho/godotenv"
+)
+
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
+	
+	_, err = sqlconnect.ConnectDb()
+	if err != nil {
+		fmt.Println("Error ....", err)
+	}
+	port := os.Getenv("API_PORT")
+
+	cert := "cert.pem"
+	key := "key.pem"
+
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	// rl := mw.NewRateLimiter(5, time.Minute)
+
+	// hppOptions := mw.HPPOptions{
+	// 	CheckQuery: true,
+	// 	CheckBody: true,
+	// 	CheckBodyOnlyForContentType: "application/x-www-form-encoded",
+	// 	WhiteList: []string{"sortBy", "sortOrder", "name", "age", "class"},
+	// }
+
+	// secureMux := mw.Cors(rl.Middleware(mw.ResponsetimeMiddleware(mw.SecurityHeaders(mw.Compression(mw.Hpp(hppOptions)(mux))))))
+	// secureMux := utils.ApplyMiddlewares(mux, mw.Hpp(hppOptions), mw.Compression, mw.SecurityHeaders, mw.ResponsetimeMiddleware, rl.Middleware, mw.Cors)
+	router := router.Router()
+	secureMux := mw.SecurityHeaders(router)
+
+	// create custom server
+	server := &http.Server{
+		Addr: port,
+		Handler: secureMux,
+		TLSConfig: tlsConfig,
+	}
+
+	fmt.Println("Server is running on port: ", port)
+	err = server.ListenAndServeTLS(cert, key)
+	if err != nil {
+		log.Fatalln("Error starting the server", err)
+	}
+}
